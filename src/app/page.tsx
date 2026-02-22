8a8e4647e2217a42859cecd180e543713a59b15a
Michael Mersiades <fakename014@gmail.com>
2026-02-22 18:30:01 +1000

    WIP - adds header and footer to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,73 @@
-import NavbarLink from '@/components/links/NavbarLink';
+import CtaLink from '@/components/links/CtaLink';
+import cn from '@/utils/cn';
+import Link from 'next/link';
+import './landing.css';
 
-export default function Home() {
+export default function Landing() {
   return (
-    <div
-      className={
-        'bg-tertiary-50 dark:bg-tertiary-950 flex items-center justify-between gap-4 p-4'
-      }
-    >
-      <NavbarLink
-        label={'Home'}
-        path={'about'}
-      />
-      <p className={'text-center font-bold'}>What is Degrowth?</p>
-      <NavbarLink
-        label={'Get Involved'}
-        path={'get-involved'}
-      />
-    </div>
+    <>
+      {/*Header*/}
+      <div className={'cta-container'}>
+        <CtaLink href={'/get-involved'}>Get Involved</CtaLink>
+      </div>
+      <header className="header">
+        <h1 className={'heading'}>What is Degrowth?</h1>
+      </header>
+
+      {/*Fallback header*/}
+      <header className="fallback-header">
+        <h1 className={'heading'}>What is Degrowth?</h1>
+        <div className={'fallback-cta-container'}>
+          <CtaLink href={'/get-involved'}>Get Involved</CtaLink>
+        </div>
+      </header>
+      <main>
+        <div className="fallback-header-spacer"></div>
+        <div className="header-spacer"></div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+        <div className={'h-20 p-20'}>.</div>
+      </main>
+
+      {/*Footer*/}
+      <>
+        <Link
+          className={'home-container'}
+          href={'/about'}
+        >
+          <span className={cn(['home-icon', 'icon-[lucide--home]'])}></span>
+        </Link>
+        <footer className={'footer'}>
+          <div className={'progress'}></div>
+        </footer>
+      </>
+
+      {/*Fallback footer*/}
+      <footer className={'fallback-footer'}>
+        <Link
+          className={'fallback-home-container'}
+          href={'/about'}
+        >
+          <span className={cn(['home-icon', 'icon-[lucide--home]'])}></span>
+        </Link>
+      </footer>
+    </>
   );
 }
