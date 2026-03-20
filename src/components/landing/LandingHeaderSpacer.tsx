a8393ab2f06b0facc83c46aea04ef80aee7351b5
Michael Mersiades <fakename014@gmail.com>
2026-03-20 17:43:49 +1000

    Adds scroll indicator to landing page

--- src/components/landing/LandingHeaderSpacer.tsx
+++ src/components/landing/LandingHeaderSpacer.tsx
@@ -1,11 +1,33 @@
-import { FC } from 'react';
+'use client';
+import { FC, useEffect, useRef } from 'react';
 import './styles.css';
 
 const LandingHeaderSpacer: FC = () => {
+  const scrollIndicatorRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollIndicatorRef.current) {
+        if (window.scrollY > 200) {
+          scrollIndicatorRef.current.classList.add('hidden');
+        } else {
+          scrollIndicatorRef.current.classList.remove('hidden');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <div className="fallback-header-spacer"></div>
       <div className="header-spacer"></div>
+      <span
+        ref={scrollIndicatorRef}
+        className={'icon-[lucide--arrow-big-down-dash] scroll-indicator'}
+      />
     </>
   );
 };
