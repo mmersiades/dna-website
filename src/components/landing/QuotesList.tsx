b6b5ce5e06566ffddfbc5ea1039d762a36a7b340
Michael Mersiades <fakename014@gmail.com>
2026-03-20 17:09:28 +1000

    Renames Degrwoth Description

--- src/components/landing/QuotesList.tsx
+++ src/components/landing/QuotesList.tsx
@@ -1,28 +1,16 @@
 import Quote from '@/components/landing/Quote';
 import { sanityFetch } from '@/sanity/lib/live';
-import { DEGROWTH_DEFINITIONS_QUERY } from '@/sanity/lib/queries';
+import { DEGROWTH_DESCRIPTIONS_QUERY } from '@/sanity/lib/queries';
 
 export default async function QuotesList() {
   const { data: quotes } = await sanityFetch({
-    query: DEGROWTH_DEFINITIONS_QUERY,
+    query: DEGROWTH_DESCRIPTIONS_QUERY,
   });
 
   const shuffledQuotes = [...quotes]
     // eslint-disable-next-line react-hooks/purity
     .sort(() => Math.random() - 0.5)
     .slice(0, 15);
-
-  // const test: DEGROWTH_DEFINITIONS_QUERYResult[0] = {
-  //   _id: 'qwerty',
-  //   citationText:
-  //     'Test test test test test Test test test test test Test test test test test Test test test test test Test test test test test Test test test test test!',
-  //   citationUrl: 'https://www.google.com',
-  //   identifier: 'Test test test test test test!',
-  //   quote:
-  //     'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test!',
-  //   statement: 'test test test test test!',
-  //   author: 'Test Test Test Test Test Test Test Test Test Test!',
-  // };
 
   return (
     <>
