d0d15f8d8d3820e6f51561d7e6af9853e55d20ed
Michael Mersiades <fakename014@gmail.com>
2026-05-02 20:20:05 +1000

    Adds CTA at bottom of landing page

---
+++ src/components/landing/LandingFooterSpacer.stories.tsx
@@ -0,0 +1,64 @@
+import LandingFooterSpacer from '@/components/landing/LandingFooterSpacer';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import centeredDecorator from '../../../.storybook/centeredDecorator';
+
+const meta = {
+  component: LandingFooterSpacer,
+  globals: {
+    viewport: { value: 'desktop', isRotated: false },
+  },
+  decorators: [centeredDecorator],
+} satisfies Meta<typeof LandingFooterSpacer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// noinspection JSUnusedGlobalSymbols
+export const DesktopLandscape: Story = {
+  globals: {
+    theme: 'light',
+  },
+};
+
+// noinspection JSUnusedGlobalSymbols
+export const DesktopLandscapeHover: Story = {
+  globals: {
+    theme: 'light',
+  },
+  parameters: {
+    pseudo: { hover: true },
+  },
+};
+
+// noinspection JSUnusedGlobalSymbols
+export const TabletLandscape: Story = {
+  globals: {
+    viewport: { value: 'tablet', isRotated: true },
+    theme: 'light',
+  },
+};
+
+// noinspection JSUnusedGlobalSymbols
+export const TabletPortrait: Story = {
+  globals: {
+    viewport: { value: 'tablet', isRotated: false },
+    theme: 'light',
+  },
+};
+
+// noinspection JSUnusedGlobalSymbols
+export const SmallMobilePortrait: Story = {
+  globals: {
+    viewport: { value: 'mobile1', isRotated: false },
+    theme: 'light',
+  },
+};
+
+// noinspection JSUnusedGlobalSymbols
+export const LargeMobilePortrait: Story = {
+  globals: {
+    viewport: { value: 'mobile2', isRotated: false },
+    theme: 'light',
+  },
+};
--- src/components/landing/LandingFooterSpacer.tsx
+++ src/components/landing/LandingFooterSpacer.tsx
@@ -1,10 +1,58 @@
+import copy from '@/constants/copy';
+import cn from '@/utils/cn';
+import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
 
 const LandingFooterSpacer: FC = () => {
+  const { cta } = copy.header;
+
+  const { link, container, image, textContainer, picture } = {
+    link: cn(
+      'flex items-center justify-center',
+      'size-70',
+      'text-center',
+      'font-bold text-3xl lg:text-4xl',
+      'font-display',
+      'hover:-translate-y-1',
+      'transition-transform',
+    ),
+    container: cn('h-[30vh]', 'flex items-center justify-center', 'mb-35'),
+    image: 'opacity-60 dark:opacity-55',
+    textContainer: cn(
+      'absolute',
+      'size-70',
+      'flex  items-center justify-center',
+    ),
+    picture: 'relative h-full w-full',
+  };
+
   return (
     <>
       <div className="fallback-header-spacer"></div>
-      <div className="h-[50vh]"></div>
+      <div className={container}>
+        <Link
+          className={link}
+          href={cta.href}
+        >
+          <picture className={picture}>
+            <source
+              srcSet={'/flower-stippled-coloured-no-outline-dark.svg'}
+              media="(prefers-color-scheme: dark)"
+            />
+            <Image
+              className={image}
+              src={'/flower-stippled-coloured-no-outline-light.svg'}
+              alt={'A folk art flower'}
+              fill
+              priority
+            />
+            <div className={textContainer}>
+              <span className="mb-20">See more</span>
+            </div>
+          </picture>
+        </Link>
+      </div>
     </>
   );
 };
